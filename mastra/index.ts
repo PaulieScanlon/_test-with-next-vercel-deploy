1a76306aa9cf2b07d5ef30599fcb5ebfa803679e
Paul Scanlon <pauliescanlon@gmail.com>
2025-06-09 13:48:37 -0700

    add vercel deployer

--- mastra/index.ts
+++ mastra/index.ts
@@ -1,18 +1,23 @@
+import { Mastra } from "@mastra/core/mastra";
+import { PinoLogger } from "@mastra/loggers";
+import { LibSQLStore } from "@mastra/libsql";
+import { VercelDeployer } from "@mastra/deployer-vercel";
 
-import { Mastra } from '@mastra/core/mastra';
-import { PinoLogger } from '@mastra/loggers';
-import { LibSQLStore } from '@mastra/libsql';
-
-import { weatherAgent } from './agents/weather-agent';
+import { weatherAgent } from "./agents/weather-agent";
 
 export const mastra = new Mastra({
   agents: { weatherAgent },
   storage: new LibSQLStore({
     // stores telemetry, evals, ... into memory storage, if it needs to persist, change to file:../mastra.db
-    url: ":memory:",
+    url: ":memory:"
   }),
   logger: new PinoLogger({
-    name: 'Mastra',
-    level: 'info',
+    name: "Mastra",
+    level: "info"
   }),
+  deployer: new VercelDeployer({
+    teamSlug: "paulie-scanlons-projects",
+    projectName: "test-with-next-vercel-deploy",
+    token: "zNrh9r6fWELjUqQdIQ5DwMck"
+  })
 });
